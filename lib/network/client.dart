9dc855ef1cb9fb2b2960eeb9845c1edbef39ffc0
InNoobWeTrust <forbidden.gun@gmail.com>
2018-04-13 12:44:40 +0700

    Add ability to use cookie to HttpClient

--- lib/network/client.dart
+++ lib/network/client.dart
@@ -2,13 +2,31 @@ import 'dart:async';
 import 'package:http/http.dart';
 
 class UserAgentClient extends BaseClient {
+  static const String _defaultUserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64)";
   final String userAgent;
   final Client _inner = new Client();
 
-  UserAgentClient([this.userAgent="Mozilla/5.0 (Windows NT 6.3; WOW64)"]);
+  UserAgentClient([this.userAgent=_defaultUserAgent]);
+
+  void processHeader(Request request) {
+    request.headers['user-agent'] = userAgent;
+  }
 
   Future<StreamedResponse> send(BaseRequest request) async {
-    request.headers['user-agent'] = userAgent;
+    processHeader(request);
     return _inner.send(request);
   }
-}
+}
+
+class MangaReaderClient extends UserAgentClient {
+  Map<String, dynamic> cookies = <String, dynamic>{};
+
+  MangaReaderClient([userAgent=UserAgentClient._defaultUserAgent, this.cookies]):super(userAgent);
+
+  @override
+  void processHeader(Request request) {
+    super.processHeader(request);
+    if(cookies.containsKey(request.url.host))
+      request.headers['Cookie'] = cookies[request.url.host];
+  }
+}
