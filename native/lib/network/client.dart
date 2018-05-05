3fefbcc53244b451fc12af69559003c70cab047b
InNoobWeTrust <forbidden.gun@gmail.com>
2018-05-05 23:03:16 +0700

    Organize project

---
+++ native/lib/network/client.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:http/http.dart';
+
+class UserAgentClient extends BaseClient {
+  static const String _defaultUserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64)";
+  final String userAgent;
+  final Client _inner = new Client();
+
+  UserAgentClient([this.userAgent = _defaultUserAgent]);
+
+  void processHeader(Request request) {
+    request.headers['user-agent'] = userAgent;
+  }
+
+  Future<StreamedResponse> send(BaseRequest request) async {
+    processHeader(request);
+    return _inner.send(request);
+  }
+}
+
+class MangaReaderClient extends UserAgentClient {
+  Map<String, dynamic> cookies = <String, dynamic>{};
+
+  MangaReaderClient(
+      [userAgent = UserAgentClient._defaultUserAgent, this.cookies])
+      : super(userAgent);
+
+  @override
+  void processHeader(Request request) {
+    super.processHeader(request);
+    if (cookies.containsKey(request.url.host))
+      request.headers['Cookie'] = cookies[request.url.host];
+  }
+}
