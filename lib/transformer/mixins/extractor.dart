5ecbf7ebb028b6a6a5a858307cbea39031dbafca
InNoobWeTrust <forbidden.gun@gmail.com>
2018-05-23 22:16:28 +0700

    - Remove pubspect.lock - Drafting the transformer classes

---
+++ lib/transformer/mixins/extractor.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import '../parser/parser.dart' show Parser;
+
+class Extractor {
+  Map<String, Parser> parsers;
+
+  Extractor setParsers(Map<String, Parser> parsers) => this..parsers = parsers;
+
+  Extractor setParser(String name, Parser parser) {
+    if (this.parsers == null) this.parsers = new Map<String, Parser>();
+    return this..parsers[name] = parser;
+  }
+
+  Future<String> transform(String parserName, String data) {
+    return this.parsers[parserName].findIn(data);
+  }
+
+  Future<List<String>> transformAll(String parserName, String data) {
+    return this.parsers[parserName].findAllIn(data);
+  }
+}
