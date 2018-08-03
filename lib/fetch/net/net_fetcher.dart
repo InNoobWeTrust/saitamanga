aca52f9c02e5c6274cde218ba87ad78fc0a9f527
InNoobWeTrust <forbidden.gun@gmail.com>
2018-08-04 01:18:34 +0700

    Split code to smaller pieces

--- lib/fetch/net/net_fetcher.dart
+++ lib/fetch/net/net_fetcher.dart
@@ -15,22 +15,37 @@ class NetFetcher extends BaseClient {
 
   Future<StreamedResponse> send(BaseRequest request) => _inner.send(request);
 
-  Future<Response> fetch(Request request) async {
+  Request _checkHeaders(Request request) {
     if (!request.headers.containsKey("user-agent"))
       request.headers["user-agent"] = this.userAgent;
     if (!request.headers.containsKey("Cookie") &&
         this.cookies.containsKey(request.link.host))
       request.headers["Cookie"] = this.cookies[request.link.host];
-    final String method = request.metadata.containsKey("method")
-        ? request.metadata["method"]
-        : "GET";
+    if (!request.metadata.containsKey("method"))
+      request.metadata["method"] = "GET";
+    return request;
+  }
+
+  StreamedRequest _streamFromRequest(Request request) {
     final StreamedRequest streamedRequest =
-        new StreamedRequest(method, request.link)
+        new StreamedRequest(request.metadata["method"], request.link)
           ..headers.addAll(request.headers);
-    if (method == "POST") request.data.listen(streamedRequest.sink.add);
-    final StreamedResponse streamedResponse = await send(streamedRequest);
-    final Response response = new Response(request, streamedResponse.stream,
+    if (request.metadata["method"] == "POST")
+      request.data.listen(streamedRequest.sink.add);
+    return streamedRequest;
+  }
+
+  Response _responseFromStream(
+      Request request, StreamedResponse streamedResponse) {
+    return new Response(request, streamedResponse.stream,
         headers: streamedResponse.headers);
+  }
+
+  Future<Response> fetch(Request request) async {
+    _checkHeaders(request);
+    final StreamedRequest streamedRequest = _streamFromRequest(request);
+    final StreamedResponse streamedResponse = await send(streamedRequest);
+    final Response response = _responseFromStream(request, streamedResponse);
     return response;
   }
 }
