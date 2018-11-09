6f018b2b045ddb71671853bbadf2fc5486f78d82
InNoobWeTrust <forbidden.gun@gmail.com>
2018-11-10 02:06:26 +0700

    Enhance the compatibility of automate scripts

--- tool/format_source.dart
+++ tool/format_source.dart
@@ -1,9 +1,9 @@
-import 'dart:io' show Process, stdout, stderr;
+import 'dart:io' show Process, stdout, stderr, Directory;
 
 void main() {
   Process.start('dartfmt', [
     '-w',
-    '.',
+    Directory.current.parent.absolute.path,
   ]).then((process) {
     stdout.addStream(process.stdout);
     stderr.addStream(process.stderr);
